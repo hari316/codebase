57bafea1231a3ff132804206a55b3cf3b547d79f
= <=>
2014-07-27 11:58:58 -0700

    Removed condition check for string length 1

--- string_manipulation/python/str1.py
+++ string_manipulation/python/str1.py
@@ -1,7 +1,5 @@
 def is_palindrome(input_str):
     str_len = len(input_str)
-    if str_len == 1:
-        return False
     for index in range(str_len/2):
         if input_str[index] != input_str[str_len - 1 - index]:
             return False
